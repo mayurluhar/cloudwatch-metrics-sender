97500c6fa060283af64f651d7c4c20a4fb04c9dc
Mayur Luhar <83022097+mayur778@users.noreply.github.com>
2023-10-14 03:55:49 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const axios = require('axios');
-const {getSystemData} = require('./systemState');
+const axios = require("axios");
+const { getSystemData } = require("./systemState");
 
 // Function to make the POST API request
 async function makePostRequest() {
-  let res = getSystemData();
-  const apiUrl = ''; // Replace with your API Gateway endpoint
+  let res = await getSystemData();
+  const apiUrl = ""; // Replace with your API Gateway endpoint
   const postData = res; // API payload
   try {
     const response = await axios.post(apiUrl, postData);
 
     if (response.status === 200) {
-      console.log('POST request successful:', response.data);
+      console.log("POST request successful:", response.data);
     } else {
-      console.error('POST request failed:', response.status, response.data);
+      console.error("POST request failed:", response.status, response.data);
     }
   } catch (error) {
-    console.error('Error making POST request:', error.message);
+    console.error("Error making POST request:", error.message);
   }
 }
 
-setInterval(makePostRequest, 60000 * 1); // set the interval according to your needs
+setInterval(makePostRequest, 5000); // set the interval according to your needs
